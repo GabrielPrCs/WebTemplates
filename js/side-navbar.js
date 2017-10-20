36b528fbb8d0d59a173fed346b780be382515301
Gabriel Prieto Cassano <gabrielprietocassano@gmail.com>
2017-10-20 13:51:20 -0300

    Changes to sidenavbar and modal

--- js/side-navbar.js
+++ js/side-navbar.js
@@ -1,20 +1,32 @@
-var toggle = function(){
-  if($('.side-navbar').hasClass('is-closed')){
-    $('.side-navbar').removeClass('is-closed')
-    $('.main-content').addClass('side-navbar-is-open');
-  }
-  else{
-    $('.side-navbar').addClass('is-closed');
-    $('.main-content').removeClass('side-navbar-is-open');
+var toggle_sidenavbar = function(){
+  if (!$('.side-navbar').hasClass('is-permanent')) {
+    if($('.side-navbar').hasClass('is-open')){
+      $('.side-navbar').removeClass('is-open')
+      $('.main-content').removeClass('side-navbar-is-open');
+    }
+    else{
+      $('.side-navbar').addClass('is-open');
+      $('.main-content').addClass('side-navbar-is-open');
+    }
   }
 }
 
-$( document ).ready(function(){
-  if(!$('.side-navbar').hasClass('is-closed')){
-    $('.side-navbar').addClass('is-closed');
+$(document).ready(function(){
+  if ($('.side-navbar').hasClass('is-permanent')) {
+    $('.side-navbar').addClass('is-open');
+    $('.main-content').addClass('side-navbar-is-open');
   }
-  $('.side-navbar-button').on('click', toggle);
-  $('.side-navbar-toggle').on('click', toggle);
-  $('.side-navbar > .side-navbar-body > .item').on('click', toggle);
+})
 
+$('.side-navbar-toggle').on('click', toggle_sidenavbar);
+$('.side-navbar > .side-navbar-body > .items-close-after-click > *').on('click', toggle_sidenavbar);
+
+$('.main-content').mouseup(function(e)
+{
+  var container = $(".side-navbar");
+  // if the target of the click isn't the container nor a descendant of the container
+  if (container.hasClass('is-open') && !container.is(e.target) && container.has(e.target).length === 0)
+  {
+    toggle_sidenavbar();
+  }
 });
