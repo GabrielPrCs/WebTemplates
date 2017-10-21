c76db706f13ae2ae5ae114830be7503a01c77ce9
Gabriel Prieto Cassano <gabrielprietocassano@gmail.com>
2017-10-21 20:22:31 -0300

    Added transition to tabs on tab change

--- js/tabs.js
+++ js/tabs.js
@@ -4,4 +4,11 @@ $('.tabs-link').on('click',function(){
   var tab_id = $(this).data('target');
   $(tab_id).siblings().removeClass('tab-active');
   $(tab_id).addClass('tab-active');
+  $(tab_id).parent().css({'height':$(tab_id).css('height')})
 })
+
+$(document).ready(function(){
+  $('.tab.tab-active').each(function(){
+    $(this).parent().css({'height':$(this).css('height')})
+  })
+});
