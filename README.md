80842cb1542dfea77742cf57a3cecb696a768ece
Gabriel Prieto Cassano <gabrielprietocassano@gmail.com>
2017-10-20 22:24:18 -0300

    Modals V1

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Designs up to day
 # Modals:
 The modal is a popup window which can be used for user notifications, UI enhancements, e-commerce components and many other cases.
 ### Full screen modal:
-A modal that appears from the top of the screen and is positioned approximately in the middle of the screen. **It is only possible to have one of these modals at a time.**
+A modal that appears from the top of the screen and is positioned approximately in the middle of the screen.
 ### Bottom modal:
-A modal that appears from the bottom up on the screen. **It is only possible to have one of these modals at a time.**
+A modal that appears from the bottom up on the screen.
--- js/modals.js
+++ js/modals.js
@@ -1,37 +1,26 @@
 var toggle_full_modal = function() {
-  if($('.full-modal').hasClass('is-open')){
-    $('.full-modal').removeClass('is-open')
+  var target = $(this).data('target');
+  if($(target).hasClass('is-open')){
+    $(target).removeClass('is-open')
     $('body').removeClass('full-modal-open');
   }
   else {
-    $('.full-modal').addClass('is-open');
+    $(target).addClass('is-open');
     $('body').addClass('full-modal-open');
   }
 }
 
 var toggle_bottom_modal = function() {
-  if($('.bottom-modal').hasClass('is-open')){
-    $('.bottom-modal').removeClass('is-open')
+  var target = $(this).data('target');
+  if($(target).hasClass('is-open')){
+    $(target).removeClass('is-open')
     $('body').removeClass('bottom-modal-open');
   }
   else {
-    $('.bottom-modal').addClass('is-open');
+    $(target).addClass('is-open');
     $('body').addClass('bottom-modal-open');
   }
 }
 
 $('.full-modal-toggle-button').on('click',toggle_full_modal);
 $('.bottom-modal-toggle-button').on('click',toggle_bottom_modal);
-
-$(document).mouseup(function(e)
-{
-  // var container = $(".full-modal");
-  // // if the target of the click isn't the container nor a descendant of the container
-  // if (container.hasClass('is-open') && !container.is(e.target) && container.has(e.target).length === 0)
-  // {
-  //   toggle_full_modal();
-  // }
-
-
-
-});
