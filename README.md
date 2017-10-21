18dcb9f44b68dd47571acf041c2166b57b7be75c
Gabriel Prieto Cassano <gabrielprietocassano@gmail.com>
2017-10-21 02:06:12 -0300

    Side-navbar V1

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Designs up to day
 
 # Card
 # Side Navigation Bar
+Bar that appears from the left side of the screen, in which you can place "items" to use as navigation within the page.
 
 # Modals:
 The modal is a popup window which can be used for user notifications, UI enhancements, e-commerce components and many other cases. Each modal has to have an ID, so a button with a data-target can toggle it.
--- js/side-navbar.js
+++ js/side-navbar.js
@@ -1,32 +1,24 @@
 var toggle_sidenavbar = function(){
-  if (!$('.side-navbar').hasClass('is-permanent')) {
-    if($('.side-navbar').hasClass('is-open')){
-      $('.side-navbar').removeClass('is-open')
-      $('.main-content').removeClass('side-navbar-is-open');
-    }
-    else{
-      $('.side-navbar').addClass('is-open');
-      $('.main-content').addClass('side-navbar-is-open');
-    }
+  if($('.side-navbar').hasClass('is-open')){
+    $('.side-navbar').removeClass('is-open')
+    $('body').removeClass('side-navbar-is-open');
+  }
+  else{
+    $('.side-navbar').addClass('is-open');
+    $('body').addClass('side-navbar-is-open');
   }
 }
 
 $(document).ready(function(){
-  if ($('.side-navbar').hasClass('is-permanent')) {
-    $('.side-navbar').addClass('is-open');
-    $('.main-content').addClass('side-navbar-is-open');
+  if ($('.side-navbar').hasClass('overlapping')) {
+    $('body').addClass('side-navbar-overlapping');
   }
 })
 
-$('.side-navbar-toggle').on('click', toggle_sidenavbar);
+$('.side-navbar-toggle-button').on('click', toggle_sidenavbar);
 $('.side-navbar > .side-navbar-body > .items-close-after-click > *').on('click', toggle_sidenavbar);
-
-$('.main-content').mouseup(function(e)
-{
-  var container = $(".side-navbar");
-  // if the target of the click isn't the container nor a descendant of the container
-  if (container.hasClass('is-open') && !container.is(e.target) && container.has(e.target).length === 0)
-  {
+$('.main-content').on('mouseup',function(){
+  if($('body').hasClass('side-navbar-is-open')){
     toggle_sidenavbar();
   }
 });
