adb265741164bf1a08654c46b791cb8dee76f434
Gabriel Prieto Cassano <gabrielprietocassano@gmail.com>
2017-10-21 20:41:10 -0300

    Tabs V1.1

--- js/tabs.js
+++ js/tabs.js
@@ -1,14 +1,18 @@
-$('.tabs-link').on('click',function(){
-  $(this).siblings().removeClass('active');
-  $(this).addClass('active');
-  var tab_id = $(this).data('target');
+var change_tab = function(button_pressed){
+  button_pressed.siblings().removeClass('active');
+  button_pressed.addClass('active');
+  var tab_id = button_pressed.data('target');
   $(tab_id).siblings().removeClass('tab-active');
   $(tab_id).addClass('tab-active');
+  // Correction of the height of the parent div (.tabs-body) since the position of .tab is absolute
   $(tab_id).parent().css({'height':$(tab_id).css('height')})
-})
+}
 
 $(document).ready(function(){
-  $('.tab.tab-active').each(function(){
-    $(this).parent().css({'height':$(this).css('height')})
+  $('.tabs-link.active').each(function(){
+    change_tab($(this));
+  });
+  $('.tabs-link').on('click',function(){
+    change_tab($(this));
   })
 });
