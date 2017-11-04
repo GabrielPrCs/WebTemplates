88162707a8ff07c512ae1b81d28f5793ce1decd6
Gabriel Prieto Cassano <gabrielprietocassano@gmail.com>
2017-11-04 00:37:29 -0300

    Tabs.js now is dynamic

--- js/tabs.js
+++ js/tabs.js
@@ -5,14 +5,15 @@ var change_tab = function(button_pressed){
   $(tab_id).siblings().removeClass('tab-active');
   $(tab_id).addClass('tab-active');
   // Correction of the height of the parent div (.tabs-body) since the position of .tab is absolute
-  // $(tab_id).parent().css({'height':$(tab_id).css('height')})
+  $(tab_id).parent().css({'height':$(tab_id).css('height')})
 }
 
 $(document).ready(function(){
   $('.tabs-link.active').each(function(){
     change_tab($(this));
   });
-  $('.tabs-link').on('click',function(){
-    change_tab($(this));
-  })
+});
+
+$(document).on('click','.tabs-link', function(){
+  change_tab($(this));
 });
